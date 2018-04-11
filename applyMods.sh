9313be409513716eb39bffd51fb5d8263da6e8d5
Eugen Cristea <eugen@cristea.org>
2018-04-11 07:43:34 +0200

    git add applyMods.sh

---
+++ applyMods.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+rm -rf obvius/content/post/*
+cp mods/obvius/themes/jane/layouts/partials/footer.html obvius/themes/jane/layouts/partials/footer.html
+cp mods/obvius/config.toml obvius/

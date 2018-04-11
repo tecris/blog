fa70c4994f99ca3fa59aed24970120280c6fd910
Eugen Cristea <eugen@cristea.org>
2018-04-11 20:45:46 +0200

    update

--- applyMods.sh
+++ applyMods.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 rm -rf obvius/content/post/*
-cp mods/obvius/themes/jane/layouts/partials/footer.html obvius/themes/jane/layouts/partials/footer.html
-cp mods/obvius/config.toml obvius/
+cp -r mods/obvius .

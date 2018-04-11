7fa5d5d85effb3317f38c82e12214857022a92f1
Eugen Cristea <eugen@cristea.org>
2018-04-11 08:02:41 +0200

    update

--- generate.sh
+++ generate.sh
@@ -2,7 +2,7 @@
 
 cd obvius;
 # generate site
-docker run --rm -v $(pwd):/src jojomi/hugo hugo
+#docker run --rm -v $(pwd):/src jojomi/hugo hugo
 # https://gohugo.io/getting-started/usage/#draft-future-and-expired-content
-# docker run --rm -v $(pwd):/src jojomi/hugo hugo --buildDrafts
+docker run --rm -v $(pwd):/src jojomi/hugo hugo --buildDrafts
 # site available in the public directory

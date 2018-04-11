305d35a3bbbe920c55d9ac5db628491546539c5f
Eugen Cristea <eugen@cristea.org>
2018-04-11 07:12:01 +0200

    enable drafts

--- generate.sh
+++ generate.sh
@@ -3,4 +3,6 @@
 cd obvius;
 # generate site
 docker run --rm -v $(pwd):/src jojomi/hugo hugo
+# https://gohugo.io/getting-started/usage/#draft-future-and-expired-content
+# docker run --rm -v $(pwd):/src jojomi/hugo hugo --buildDrafts
 # site available in the public directory

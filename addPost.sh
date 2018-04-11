b83d24e2a38efce7beada5ba9d270d7df304a617
Eugen Cristea <eugen@cristea.org>
2018-04-11 07:43:56 +0200

    update

--- addPost.sh
+++ addPost.sh
@@ -2,3 +2,5 @@
 
 cd obvius;
 docker run --rm -v $(pwd):/src jojomi/hugo hugo new content/post/it-started-here.md
+# docker run --rm -v $(pwd):/src jojomi/hugo hugo new content/post/post-uno.md
+# docker run --rm -v $(pwd):/src jojomi/hugo hugo new content/post/post-due.md

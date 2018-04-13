627f592792e161cfd6036cab62feddcad84b8bf9
Eugen Cristea <eugen@cristea.org>
2018-04-13 19:41:03 +0200

    cleanup

--- 01-init.sh
+++ 01-init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker run --rm --name "hugo-blog" -P -v $(pwd):/src jojomi/hugo hugo new site obvius
-docker run --rm --name "hugo-blog" -P -v $(pwd):/src jojomi/hugo chown -R 1000:1000 obvius
+docker run --rm --name "hugo-blog" -v $(pwd):/src jojomi/hugo hugo new site obvius
+docker run --rm -v $(pwd):/src jojomi/hugo chown -R 1000:1000 obvius
 cd obvius/
 git clone https://github.com/xianmin/hugo-theme-jane.git --depth=1 themes/jane
 cp -r themes/jane/exampleSite/content ./

0c33b84bd0185665198c8179a9fb12b6f07c55b0
Eugen Cristea <eugen@cristea.org>
2018-04-13 07:08:18 +0200

    run maven in docke

--- 15-addPost.sh
+++ 15-addPost.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+# usage ./addPost.sh post-title-here
+
 cd obvius;
-docker run --rm -v $(pwd):/src jojomi/hugo hugo new content/post/it-started-here.md
+docker run --rm -v $(pwd):/src jojomi/hugo hugo new content/post/$1.md
 # docker run --rm -v $(pwd):/src jojomi/hugo hugo new content/post/post-uno.md
 # docker run --rm -v $(pwd):/src jojomi/hugo hugo new content/post/post-due.md
---
+++ mods/obvius/content/post/run-maven-in-docker.md
@@ -0,0 +1,7 @@
+---
+title: "Run Maven in Docker"
+date: 2018-04-13T04:53:46Z
+draft: true
+---
+
+Assuming docker and/or docker-compose are available:

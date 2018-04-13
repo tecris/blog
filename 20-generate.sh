85df2b1bf4a9ce37dea3291cfbcd875ab6e72bd7
Eugen Cristea <eugen@cristea.org>
2018-04-13 19:41:18 +0200

    cleanup fix

--- 20-generate.sh
+++ 20-generate.sh
@@ -5,4 +5,5 @@ cd obvius;
 #docker run --rm -v $(pwd):/src jojomi/hugo hugo
 # https://gohugo.io/getting-started/usage/#draft-future-and-expired-content
 docker run --rm -v $(pwd):/src jojomi/hugo hugo --buildDrafts
+docker run --rm -v $(pwd):/src jojomi/hugo chown -R 1000:1000 obvius
 # site available in the public directory

11ef997a3f7a5defa614c3396294500501cbb61e
Eugen Cristea <eugen@cristea.org>
2018-04-14 17:25:28 +0200

    permissions fix

--- 20-generate.sh
+++ 20-generate.sh
@@ -5,5 +5,5 @@ cd trivialis-blog;
 #docker run --rm -v $(pwd):/src jojomi/hugo hugo
 # https://gohugo.io/getting-started/usage/#draft-future-and-expired-content
 docker run --rm -v $(pwd):/src jojomi/hugo hugo --buildDrafts
-docker run --rm -v $(pwd):/src jojomi/hugo chown -R 1000:1000 trivialis-blog
+docker run --rm -v $(pwd):/src jojomi/hugo chown -R 1000:1000 public
 # site available in the public directory
